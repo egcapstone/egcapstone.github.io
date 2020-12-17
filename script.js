1993f5435231f07e93e865e29e93a193e9ff4eb9
egcapstone <76144848+egcapstone@users.noreply.github.com>
2020-12-17 14:17:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function startLiveUpdate(){
+		const textHeadCount = document.getElementById('headCount');
+		const textExceed = document.getElementById('exceed');
+		
+		setInterval(function(){
+			fetch('http://blynk-cloud.com/YeaI5w0Tqlrr4wfL3YdAZ_GcUqCYs9le/get/V0').then(function(response){
+				return response.json();
+				}).then(function(data){
+					textHeadCount.textContent = data;
+					if (data > 30){
+						textExceed.textContent = "The number of people have exceeded!";
+					}
+				}).catch(function(error){
+					console.log(error);
+				})
+			}, 2000);
+		}
+		
+document.addEventListener('DOMContentLoaded', function(){
+	startLiveUpdate();
+	});

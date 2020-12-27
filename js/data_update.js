452878ff0dbcbe71c2965a0c856483327528aa6c
egcapstone <76144848+egcapstone@users.noreply.github.com>
2020-12-28 00:30:57 +0800

    Add files via upload

---
+++ js/data_update.js
@@ -0,0 +1,20 @@
+function startLiveUpdate(){
+	const textHeadCount = document.getElementById('headCount');
+	const textExceed = document.getElementById('exceed');
+	
+	setInterval(function(){
+		fetch('https://cors-anywhere.herokuapp.com/http://188.166.206.43:8080/YeaI5w0Tqlrr4wfL3YdAZ_GcUqCYs9le/get/V0').then(function(response){
+			return response.json();
+			}).then(function(data){
+				textHeadCount.textContent = data;
+				if (data > 30){
+					textExceed.textContent = "The number of people have exceeded!";
+				}
+				else {
+					textExceed.textContent = "Within Limit!";
+				}
+			}).catch(function(error){
+				console.log(error);
+			})
+		}, 2000);
+	}	

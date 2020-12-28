a4d08567e3ed236b2ed6d050f72898e13fe1c9e4
egcapstone <76144848+egcapstone@users.noreply.github.com>
2020-12-28 19:18:28 +0800

    Update data_update.js

--- js/data_update.js
+++ js/data_update.js
@@ -3,7 +3,7 @@ function startLiveUpdate(){
 	const textExceed = document.getElementById('exceed');
 	
 	setInterval(function(){
-		fetch('https://thingproxy.freeboard.io/fetch/http://188.166.206.43:8080/YeaI5w0Tqlrr4wfL3YdAZ_GcUqCYs9le/get/V0').then(function(response){
+		fetch('https://cors-anywhere.herokuapp.com/http://188.166.206.43:8080/YeaI5w0Tqlrr4wfL3YdAZ_GcUqCYs9le/get/V0').then(function(response){
 			return response.json();
 			}).then(function(data){
 				textHeadCount.textContent = data;

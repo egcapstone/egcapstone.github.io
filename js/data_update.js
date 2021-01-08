249c5a19eebc77950dcf61bb403a7d256f07fadc
egcapstone <76144848+egcapstone@users.noreply.github.com>
2021-01-08 20:10:21 +0800

    Update data_update.js

--- js/data_update.js
+++ js/data_update.js
@@ -1,17 +1,26 @@
 function startLiveUpdate(){
 	const textHeadCount = document.getElementById('headCount');
 	const textExceed = document.getElementById('exceed');
+	const textLimit = document.getElementById('limit');
+	
+	fetch('https://ancient-lab-fced.egcapstone.workers.dev/?http://blynk-cloud.com/YeaI5w0Tqlrr4wfL3YdAZ_GcUqCYs9le/get/V1').then(function(response){
+			return response.json();
+			}).then(function(data){
+				textLimit.textContent = data;
+			}).catch(function(error){
+				console.log(error);
+			})
 	
 	setInterval(function(){
 		fetch('https://ancient-lab-fced.egcapstone.workers.dev/?http://blynk-cloud.com/YeaI5w0Tqlrr4wfL3YdAZ_GcUqCYs9le/get/V0').then(function(response){
 			return response.json();
 			}).then(function(data){
 				textHeadCount.textContent = data;
-				if (data > 30){
-					textExceed.textContent = "The number of people have exceeded!";
+				if (data > textLimit){
+					textExceed.textContent = "Please come at another time! We are sorry!";
 				}
 				else {
-					textExceed.textContent = "Within Limit!";
+					textExceed.textContent = "Welcome!";
 				}
 			}).catch(function(error){
 				console.log(error);
